76bf0fe885e7e3686a667c7a13bf03ddf619ec8b
davidabutler92 <davidabutler92@yahoo.com>
2020-04-01 11:38:59 -0700

    add user interface logic in javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// user interface logic below
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#results").text(result);
+  });
+});
+
+leapYear(function)
